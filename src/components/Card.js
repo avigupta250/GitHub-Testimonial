8489b91d643d37bf4ef27c6855e6c47666f6a8bd
avigupta250 <avinashkumarnav52@gmail.com>
2023-10-14 02:05:37 +0530

    Added Suprise

---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+
+
+const Card =(props)=>{
+
+    let review=props.review;
+
+    return (
+        <div className="flex flex-col relative ">
+            <div className="absolute top-[-7rem] z-[10] mx-auto">
+                <img className="aspect-square rounded-full w-[140px] h-[140px]  "
+                src={review.image}/>
+                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute
+                top-[-6px] left-[10px] z-[-1]"></div>
+            </div>
+
+            <div className="text-center mt-7">
+                <p className="font-bold text-2xl capitalize">{review.name}</p>
+                <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+            </div>
+
+            <div className="text-violet-400 mx-auto mt-5">
+                <FaQuoteLeft/>
+            </div>
+
+            <div>
+                {review.text}
+            </div>
+
+            <div  className="text-violet-400 mx-auto mt-5">
+                <FaQuoteRight/>
+            </div>
+
+           
+        </div>
+    )
+}
+
+
+export default Card;
